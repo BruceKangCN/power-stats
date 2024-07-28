9c7af3c2a3c5db8e49ba8ab1092f933fefda40d5
Bruce Kang <brucekangcn@gmail.com>
2024-07-29 06:07:36 +0800

    extract main function codes to smaller functions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,10 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 
-use chrono::{NaiveDateTime, Timelike};
+use chrono::{NaiveDateTime, TimeDelta, Timelike};
+use csv::Trim;
+use model::{PowerFigureRecord, Record, WorkFigureRecord};
+
+pub mod model;
 
 /// 时段分类
 #[derive(PartialEq, Eq)]
@@ -13,10 +17,13 @@ pub enum PeriodCategory {
     NoonOffPeak,
     /// 午峰时
     NoonPeak,
-    ///其他时段
-    Other,
+    /// 平时
+    Normal,
 }
 
+/// 根据文件路径读取数据。
+///
+/// 自动判断文件的编码并转换为 UTF-8 编码格式字符串返回。
 pub fn get_file_content(filepath: &str) -> Result<String, String> {
     let matches = charset_normalizer_rs::from_path(&PathBuf::from(filepath), None)?;
     let best_match = matches.get_best().ok_or("failed to get best encoding")?;
@@ -27,6 +34,147 @@ pub fn get_file_content(filepath: &str) -> Result<String, String> {
     Ok(content.into())
 }
 
+/// 将数据映射成 HashMap 以便于下一步处理。
+pub fn map_records(content: &str) -> Result<HashMap<NaiveDateTime, f64>, String> {
+    // 读取时需清除空白字符，不然会导致数值解析出错
+    let mut reader = csv::ReaderBuilder::new()
+        .trim(Trim::All)
+        .from_reader(content.as_bytes());
+
+    let mut map = HashMap::new();
+    for result in reader.deserialize() {
+        let record: Record = result.or(Err("failed to deserialize record"))?;
+
+        let dt = NaiveDateTime::parse_from_str(&record.time, "%Y-%m-%d %H:%M:%S")
+            .expect("failed to parse datetime");
+
+        // 注意：如果有重复数据，新数据会覆盖旧数据
+        map.insert(dt, record.active_power);
+    }
+
+    Ok(map)
+}
+
+/// 生成功率数据
+pub fn get_power_data(
+    power_vec: &Vec<f64>,
+    start_point: &NaiveDateTime,
+    rated_capacity: f64,
+) -> Vec<PowerFigureRecord> {
+    let mut power_data = Vec::new();
+
+    let start_point = *start_point;
+    for (i, p) in power_vec.iter().enumerate() {
+        let dt = start_point + TimeDelta::minutes((i as i64) * 15);
+        let cat = categorize_by_datetime(&dt);
+
+        power_data.push(match cat {
+            PeriodCategory::EveningOffPeak => PowerFigureRecord {
+                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
+                evening_off_peak: Some(*p),
+                morning_peak: None,
+                noon_off_peak: None,
+                noon_peak: None,
+                evening_remain: Some(rated_capacity - *p),
+                noon_remain: None,
+            },
+            PeriodCategory::MorningPeak => PowerFigureRecord {
+                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
+                evening_off_peak: None,
+                morning_peak: Some(*p),
+                noon_off_peak: None,
+                noon_peak: None,
+                evening_remain: None,
+                noon_remain: None,
+            },
+            PeriodCategory::NoonOffPeak => PowerFigureRecord {
+                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
+                evening_off_peak: None,
+                morning_peak: None,
+                noon_off_peak: Some(*p),
+                noon_peak: None,
+                evening_remain: None,
+                noon_remain: Some(rated_capacity - *p),
+            },
+            PeriodCategory::NoonPeak => PowerFigureRecord {
+                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
+                evening_off_peak: None,
+                morning_peak: None,
+                noon_off_peak: None,
+                noon_peak: Some(*p),
+                evening_remain: None,
+                noon_remain: None,
+            },
+            PeriodCategory::Normal => PowerFigureRecord {
+                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
+                evening_off_peak: None,
+                morning_peak: None,
+                noon_off_peak: None,
+                noon_peak: None,
+                evening_remain: None,
+                noon_remain: None,
+            },
+        });
+    }
+
+    power_data
+}
+
+/// 生成能耗数据
+pub fn get_work_data(
+    power_vec: &Vec<f64>,
+    start_point: &NaiveDateTime,
+    time_delta: &TimeDelta,
+) -> Vec<WorkFigureRecord> {
+    let start_point = *start_point;
+    let time_delta = *time_delta;
+
+    let days = time_delta.num_days();
+    let mut work_data = Vec::new();
+    // 填写能耗数据
+    for day in start_point.date().iter_days().take((days + 1) as usize) {
+        // 当前步骤所计算的日期的 0 时相对于起始时间点的时长，以 15 分钟为单位
+        let offset = (day.and_hms_opt(0, 0, 0).unwrap() - start_point).num_minutes() / 15;
+
+        let max_idx = (power_vec.len() - 1) as i64;
+
+        let morning_peak = if offset + 8 * 4 > max_idx || offset + 11 * 4 <= 0 {
+            0.0
+        } else {
+            let begin = std::cmp::max(offset + 8 * 4, 0) as usize;
+            let end = std::cmp::min(offset + 11 * 4 - 1, max_idx) as usize;
+
+            power_vec[begin..end].iter().sum::<f64>() * 0.25
+        };
+
+        let noon_peak = if offset + 13 * 4 > max_idx || offset + 17 * 4 <= 0 {
+            0.0
+        } else {
+            let begin = std::cmp::max(offset + 13 * 4, 0) as usize;
+            let end = std::cmp::min(offset + 17 * 4 - 1, max_idx) as usize;
+
+            power_vec[begin..end].iter().sum::<f64>() * 0.25
+        };
+
+        work_data.push(WorkFigureRecord {
+            date: day.format("%Y-%m-%d").to_string(),
+            morning_peak,
+            noon_peak,
+        });
+    }
+
+    work_data
+}
+
+/// 根据时间返回时段分类。
+///
+/// 注：以下均为左闭右开区间
+///
+/// - 晚谷时：0 - 8
+/// - 早峰时：8 - 11
+/// - 午谷时：11 - 13
+/// - 午峰时：13 - 17
+/// - 平时：其他时段
 pub fn categorize_by_datetime(dt: &NaiveDateTime) -> PeriodCategory {
     // 注意：0 <= dt.hour() 必定为 true，写到条件表达式中会报警告
     if dt.hour() < 8 {
@@ -45,5 +193,5 @@ pub fn categorize_by_datetime(dt: &NaiveDateTime) -> PeriodCategory {
         return PeriodCategory::NoonPeak;
     }
 
-    return PeriodCategory::Other;
+    return PeriodCategory::Normal;
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::collections::HashMap;
-
-use chrono::{NaiveDateTime, TimeDelta};
-use csv::Trim;
-use model::{PowerFigureRecord, Record, RespondData, WorkFigureRecord};
-use power_stats::{categorize_by_datetime, get_file_content, PeriodCategory};
-
-pub mod model;
+use power_stats::model::RespondData;
+use power_stats::{get_file_content, get_power_data, get_work_data, map_records};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -25,127 +19,30 @@ async fn build_datasets(
     filepath: String,
 ) -> Result<RespondData, String> {
     let content = get_file_content(&filepath)?;
-    // 读取时需清除空白字符，不然会导致数值解析出错
-    let mut reader = csv::ReaderBuilder::new()
-        .trim(Trim::All)
-        .from_reader(content.as_bytes());
-
-    // 用于存放时刻-功率对应关系的哈希表
-    let mut map = HashMap::new();
-    for result in reader.deserialize() {
-        let record: Record = result.or(Err("failed to deserialize record"))?;
-
-        let dt = NaiveDateTime::parse_from_str(&record.time, "%Y-%m-%d %H:%M:%S")
-            .expect("failed to parse datetime");
-
-        map.insert(dt, record.active_power);
-    }
+    let map = map_records(&content)?;
 
     // 将 map 排序并放入 vector 中
     //
     // 注：数据中可能有缺失的记录，所以不能直接将 map.len() 作为向量的长度，
-    // 而是应计算时间间隔进而计算向量长度。
+    // 而是应计算时间间隔进而计算向量长度。缺失的记录会被填充为 0。
+    //
+    // PS: 该步骤同时还计算出了许多后续步骤需要用到的变量，不适合提取为函数。
     let start_point = map.keys().min().unwrap().to_owned();
     let time_delta = *map.keys().max().unwrap() - start_point;
-    let mut power_vec = vec![0.0; (time_delta.num_minutes() as usize) / 15 + 1];
     let factor = if is_primary_load {
         factor.unwrap()
     } else {
         1.0
     };
+    let mut power_vec = vec![0.0; (time_delta.num_minutes() as usize) / 15 + 1];
     for (k, v) in map.iter() {
         // 采样间隔为 15 分钟，所以索引值为经过的时间（以分钟为单位）除以 15
         let idx = (*k - start_point).num_minutes() / 15;
         power_vec[idx as usize] = *v * factor;
     }
 
-    // 填写功率数据
-    let mut power_data = Vec::new();
-    for (i, p) in power_vec.iter().enumerate() {
-        let dt = start_point + TimeDelta::minutes((i as i64) * 15);
-        let cat = categorize_by_datetime(&dt);
-        power_data.push(match cat {
-            PeriodCategory::EveningOffPeak => PowerFigureRecord {
-                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
-                evening_off_peak: Some(*p),
-                morning_peak: None,
-                noon_off_peak: None,
-                noon_peak: None,
-                evening_remain: Some(rated_capacity - *p),
-                noon_remain: None,
-            },
-            PeriodCategory::MorningPeak => PowerFigureRecord {
-                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
-                evening_off_peak: None,
-                morning_peak: Some(*p),
-                noon_off_peak: None,
-                noon_peak: None,
-                evening_remain: None,
-                noon_remain: None,
-            },
-            PeriodCategory::NoonOffPeak => PowerFigureRecord {
-                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
-                evening_off_peak: None,
-                morning_peak: None,
-                noon_off_peak: Some(*p),
-                noon_peak: None,
-                evening_remain: None,
-                noon_remain: Some(rated_capacity - *p),
-            },
-            PeriodCategory::NoonPeak => PowerFigureRecord {
-                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
-                evening_off_peak: None,
-                morning_peak: None,
-                noon_off_peak: None,
-                noon_peak: Some(*p),
-                evening_remain: None,
-                noon_remain: None,
-            },
-            PeriodCategory::Other => PowerFigureRecord {
-                time: dt.format("%Y-%m-%d %H:%M:00").to_string(),
-                evening_off_peak: None,
-                morning_peak: None,
-                noon_off_peak: None,
-                noon_peak: None,
-                evening_remain: None,
-                noon_remain: None,
-            },
-        });
-    }
-
-    let days = time_delta.num_days();
-    let mut work_data = Vec::new();
-    // 填写能耗数据
-    for day in start_point.date().iter_days().take((days + 1) as usize) {
-        // 当前步骤所计算的日期的 0 时相对于起始时间点的时长，以 15 分钟为单位
-        let offset = (day.and_hms_opt(0, 0, 0).unwrap() - start_point).num_minutes() / 15;
-
-        let max_idx = (map.len() - 1) as i64;
-
-        let morning_peak = if offset + 8 * 4 > max_idx || offset + 11 * 4 <= 0 {
-            0.0
-        } else {
-            let begin = std::cmp::max(offset + 8 * 4, 0) as usize;
-            let end = std::cmp::min(offset + 11 * 4 - 1, max_idx) as usize;
-
-            power_vec[begin..end].iter().sum()
-        } * 0.25;
-
-        let noon_peak = if offset + 13 * 4 > max_idx || offset + 17 * 4 <= 0 {
-            0.0
-        } else {
-            let begin = std::cmp::max(offset + 13 * 4, 0) as usize;
-            let end = std::cmp::min(offset + 17 * 4 - 1, max_idx) as usize;
-
-            power_vec[begin..end].iter().sum()
-        } * 0.25;
-
-        work_data.push(WorkFigureRecord {
-            date: day.format("%Y-%m-%d").to_string(),
-            morning_peak,
-            noon_peak,
-        });
-    }
+    let power_data = get_power_data(&power_vec, &start_point, rated_capacity);
+    let work_data = get_work_data(&power_vec, &start_point, &time_delta);
 
     Ok(RespondData {
         power_data,
